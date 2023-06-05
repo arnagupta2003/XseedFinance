b5a72fed34885fc785b1dd722b36cd0540ab8fae
Siddhant Jain <sjain2531@gmail.com>
2023-06-06 01:00:29 +0530

    Delete:Income Tax Cal

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import NavBar from './Components/navbar';
 import SipCal from './Non-Components/sip calculator';
 import FDCal from './Non-Components/fd calculator';
 import DreamCal from './Non-Components/dream cal';
 import LearningSection from './Non-Components/learning section';
 import Tools from './Non-Components/toolsPage';
 import CryptoCal from './Non-Components/cryptoTax';
-import IncomeTax from './Non-Components/incomeTaxCal';
-import { Section1,Section2,Section3,Section4 } from './Non-Components/IncomeTaxSections';
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,11 +16,6 @@ export default function App() {
           <Route path="sipCal" element={<SipCal />} />
           <Route path="DreamCal" element={<DreamCal />} />
           <Route path="cryptoCal" element={<CryptoCal />} />
-          <Route path="incomeTax" element={<IncomeTax />} />
-          {/* <Route path="incomeTax/section1" element={<Section1/>} /> */}
-          <Route path="incomeTax/section2" element={<Section2/>} />
-          <Route path="incomeTax/section3" element={<Section3/>} />
-          <Route path="incomeTax/section4" element={<Section4/>} />
           <Route path='tools' element={<Tools />} />
           {/* <Route path="*" element={<NoPage />} /> */}
           {/* <Redirect to="/" /> */}
