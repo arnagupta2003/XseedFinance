2ea686d0deb69fd19dbcc98f033d134ce0a4dbf4
siddhantjain <siddhant.j@cred.club>
2023-06-23 14:52:55 +0530

    Remove: extra files

--- backend/models/newsPost.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const newsPostSchema = new mongoose.Schema({
-  message: { type: String, required: true },
-  likes: { type: Number },
-  comments: { type: Array },
-});
--- ejs_integration/public/extra js/blogEditor.js
+++
@@ -1,7 +0,0 @@
-ClassicEditor.create(document.querySelector("#editor"))
-  .then((newEditor) => {
-    editor = newEditor;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
